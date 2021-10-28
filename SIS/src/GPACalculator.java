b9c676a349ff4283f45c3ec6ec5d15f8dd7b6a00
andrew.deberardinis <andrew.deberardinis@MU-C1-06.mullen.pvt.k12.co.us>
2021-10-28 11:46:41 -0600

    End of class OCT 28

---
+++ SIS/src/GPACalculator.java
@@ -0,0 +1,78 @@
+
+public class GPACalculator
+	{
+
+		double firstGPA = 0.00;
+		
+		public static void GPACounter()
+		{
+		
+					if(studentlist.get(i).getFirstgrade().equals("A","A+"))
+						{
+							firstGPA = firstGPA + 4.00;
+						}
+					
+					else if(studentlist.get(i).getFirstgrade().equals("A-"))
+						{
+							firstGPA = firstGPA + 3.70;
+						}
+					
+					else if(studentlist.get(i).getFirstgrade().equals("B+"))
+						{
+							firstGPA = firstGPA + 3.30;
+						}
+					
+					else if(studentlist.get(i).getFirstgrade().equals("B"))
+						{
+							firstGPA = firstGPA + 3.00;
+						}
+					
+					else if(studentlist.get(i).getFirstgrade().equals("B-"))
+						{
+							firstGPA = firstGPA + 2.70;
+						}
+					
+					else if(studentlist.get(i).getFirstgrade().equals("C+"))
+						{
+							firstGPA = firstGPA + 2.30;
+						}
+					
+					else if(studentlist.get(i).getFirstgrade().equals("C"))
+						{
+							firstGPA = firstGPA + 2.00;
+						}
+					
+					else if(studentlist.get(i).getFirstgrade().equals("C-"))
+						{
+							firstGPA = firstGPA + 1.70;
+						}
+					
+					else if(studentlist.get(i).getFirstgrade().equals("D+"))
+						{
+							firstGPA = firstGPA + 1.30;
+						}
+					
+					else if(studentlist.get(i).getFirstgrade().equals("D"))
+						{
+							firstGPA = firstGPA + 1.00;
+						}
+					
+					else if(studentlist.get(i).getFirstgrade().equals("F"))
+						{
+							firstGPA = firstGPA + 0.00;
+						}
+					
+					
+			
+			
+			
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+	}
