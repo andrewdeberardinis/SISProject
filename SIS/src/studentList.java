52f957038ab49ec49270f9db57ad83bc098e6337
samuel.ficsor <samuel.ficsor@MU-C1-14.mullen.pvt.k12.co.us>
2021-10-28 11:40:59 -0600

    started working on the arraylist to fill the POJO

---
+++ SIS/src/studentList.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class studentList
+	{
+		static ArrayList<Student> listOfStudents = new ArrayList<Student>();
+		static String studentFirstName;
+		static String studentLastName;
+		static double studentGPA;
+		static String studentFirstPeriod;
+		static String studentSecondPeriod;
+		static String studentThirdPeriod;
+		static String studentFirstPeriodGrade;
+		static String studentSecondPeriodGrade;
+		static String studentThirdPeriodGrade;
+	}
